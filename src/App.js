35b9fcead6ebd1ff2ccf672da9a1579d2611bee1
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-09-16 14:13:11 -0700

    added a few things

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className="container">
+      Use My Tech Stuff
     </div>
   );
 }
