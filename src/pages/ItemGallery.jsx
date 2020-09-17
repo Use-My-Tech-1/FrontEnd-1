f7612b75761d684f5954e40a27a9162488d93b2d
Bruno Paula <brunopaula@dryserv.com>
2020-09-17 15:25:47 -0400

    Imported Item & Gallery Components

--- src/pages/ItemGallery.jsx
+++ src/pages/ItemGallery.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Item from "./Item";
+import { dummyData } from "../dummyData";
 function ItemGallery() {
   return (
     <>
-      <div className='centerWrapper'>
+      <div className="centerWrapper">
         <h1>Gallery</h1>
+        <div className="rental-items">
+          {dummyData.map((item) => (
+            <Item data={item} />
+          ))}
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default ItemGallery
+export default ItemGallery;
