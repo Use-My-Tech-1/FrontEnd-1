8391cb59afeb38a68de761b84382335d98a25154
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-09-16 18:01:01 -0700

    created login component, created styling for form tag, created axiosWithAuth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,111 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import * as yup from 'yup';
-import Input from './Input'
+import Input from './Input';
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
+function Login(props) {
+
+    let history = useHistory();
+
+    const defaultState = {
+        username: '',
+        password: ''
+    };
+
+    const [errors, setErrors] = useState({...defaultState});
+    const [login, setLogin] = useState(defaultState);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    let formSchema = yup.object().shape({
+        username: yup
+            .string()
+            .required('Username is required')
+            .min(8, "Username must be 8 characters"),
+        password: yup
+            .string()
+            .required('Password is required')
+            .min(8, "Password must be 8 characters"),
+    });
+
+    useEffect(() => {
+        if (login.username && login.password) {
+            setButtonDisabled(false);
+        }
+        else if (!login.username || !login.password) {
+            setButtonDisabled(true);
+        }
+    }, [login]);
+
+    const loginSubmit = e => {
+        e.preventDefault();
+        console.log('login submitted');
+
+        axiosWithAuth()
+        .post('auth/login', login, {withCredentials: true})
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('token', '12345')
+            props.setLoggedIn(true);
+            history.push('/tech-items');
+        })
+        .catch(err => {
+            console.log('invalid login', err);
+        })
+    };
+
+    //onChange function, put validation within function
+    const inputChange = e => {
+        e.persist();
+        let value = e.target.value;
+        yup.reach(formSchema, e.target.name)
+        .validate(value)
+        .then(valid => {
+            setErrors({
+                ...errors,
+                [e.target.name]: ""
+            })
+        })
+        .catch(err => {
+            setErrors({
+            ...errors,
+            [e.target.name]: err.errors[0]
+            })
+        })
+        //spread accross loginState and set to value of input. 
+        setLogin({
+            ...login,
+            [e.target.name]: e.target.value
+        });
+        console.log(login);
+        // validateChange(e);
+    };
+
+    return (
+        <div className='formContainer'>
+            <form onSubmit={loginSubmit}>
+                <Input
+                    type="text"
+                    name="username"
+                    onChange={inputChange}
+                    value={login.username}
+                    label="User Name"
+                    errors={errors}
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    onChange={inputChange}
+                    value={login.password}
+                    label="Password"
+                    errors={errors}
+                />
+                <button disabled={buttonDisabled}>Login</button>
+            </form>
+        </div>
+    )
+};
+
+
+export default Login;
