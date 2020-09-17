34e5b8020f43cfacde424b7208cc457d3fe65371
Harrison-Aguiar <harryaguiar@gmail.com>
2020-09-16 21:56:28 -0400

    added sign up route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Login from './components/Login'
 import {Route, Switch} from 'react-router-dom'
 import ItemGallery from './pages/ItemGallery'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <div className='mainbody'>
         <Switch>
+          <Route path='/signup' component={SignUpForm} />
           <Route path='/itemgallery' component={ItemGallery} />
           <Route path='/login'>
             <Login setLoggedIn={setLoggedIn} />
