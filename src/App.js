df3e18569d81f59a46f0bfaa270fb8951f916032
Harrison-Aguiar <harryaguiar@gmail.com>
2020-09-16 21:42:10 -0400

    added sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import SignUpForm from './components/SignUpForm';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <SignUpForm />
     </div>
   );
 }
