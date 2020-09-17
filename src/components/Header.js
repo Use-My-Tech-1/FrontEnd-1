ac1ff1b73877f0c26f2fdadadeec21f6cb44b600
Bruno Paula <brunopaula@dryserv.com>
2020-09-17 01:23:45 -0400

    Fixed some merge conflit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
-    return (
-        <nav>
-        <h1 className="pageHeader">Rent My Tech</h1>
-        <div className='nav-links'>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/tech-items">Tech2Rent</Link>
-          <Link to="/createAuction">Add-Tech-Item</Link>
-        </div>
-      </nav>
-    )
-};
+  return (
+    <nav>
+      <h1 className='pageHeader'>Rent My Tech</h1>
+      <div className='nav-links'>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Register</Link>
+        <Link to='/tech-items'>Tech2Rent</Link>
+        <Link to='/createAuction'>Add-Tech-Item</Link>
+      </div>
+    </nav>
+  )
+}
 
-export default Header;
+export default Header
