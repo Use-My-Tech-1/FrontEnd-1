e07c2ed75b27dfeb402f003426eefd9b3898b6fe
Bruno Paula <brunopaula@dryserv.com>
2020-09-16 21:24:18 -0400

    added Item card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
+import React from 'react'
+import Header from './components/Header'
+import ItemGallery from './pages/ItemGallery.jsx'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/ItemGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemGallery() {
+  return (
+    <div>
+      <h1>Item Gallery</h1>
+    </div>
+  )
+}
+
+export default ItemGallery
