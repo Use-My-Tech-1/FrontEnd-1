60bc3c3c61e1d8b93315426c25eed50facbc44b8
Bruno Paula <brunopaula@dryserv.com>
2020-09-21 18:54:34 -0400

    Added Context API to handle User Data and login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { UserProvider } from "./context/userContext";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <UserProvider>
+    <Router>
+      <App />
+    </Router>
+  </UserProvider>,
+  document.getElementById("root")
 );
--- src/pages/ItemGallery.jsx
+++ src/pages/ItemGallery.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Item from "./Item";
 import { dummyData } from "../dummyData";
+import { UserContext } from "../context/userContext";
+
 function ItemGallery() {
+  const { userData, setUserData } = useContext(UserContext);
+
   return (
     <>
       <div className="centerWrapper">
-        <h1>Gallery</h1>
+        <h1 className="welcome--name">Welcome {userData.user.name}</h1>
         <div className="rental-items">
           {dummyData.map((item) => (
             <Item data={item} />
